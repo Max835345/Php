5ef919391f248111fbdef39255808047fa1e3c41
Max <79370059+Max835345@users.noreply.github.com>
2022-08-20 17:46:12 +0700

    Add files via upload

---
+++ gb php/task_2.php
@@ -0,0 +1,74 @@
+<?php
+// 1 задание
+//echo "В каком году произошло крещение Руси?";
+//echo "\n";
+
+//$option_1 = (string)810;
+//$option_2 = (string)990;
+//$option_3 = (string)740;
+
+//echo $option_1;
+//echo "\n";
+//echo $option_2;
+//echo "\n";
+//echo $option_3;
+//echo "\n";
+
+//$answer = readline('Напишите вариант ответа ');
+
+//if ($answer == $option_1) { 
+//    echo'Ответ не верный';
+//}
+//elseif ($answer == $option_2) { //
+//    echo'Ответ верный';
+//}
+//elseif ($answer == $option_3) { //
+//    echo'Ответ не верный';
+//} else {//
+//    echo 'Введите коректный ответ';
+//}
+
+// 2 задание
+
+// echo "Введите кол-во заданий на сегодня";
+// echo "\n";
+
+// $count = readline('Напишите кеол-вол заданий ');
+
+// for ($i = 1; $i <= $count; $i++) {
+//     echo 'Задание '.$i;
+//     echo "\n";
+//     $task = readline('Напишите заданиe ');
+//     $time = (int)readline('Напишите время ');
+//     $merr = $time;
+//     $sum =$sum+$time;
+//     $merr = 0;
+// };
+
+// echo "Всего ".$sum." часов";
+
+//3 задание
+
+// $thumb = 1;
+// $forefinger = 2;
+// $middle = 3;
+// $noname = 4;
+// $little = 5;
+
+// $some_finger = (int)readline('Напишите номер пальца от 1 до 5 ');
+
+// if ($some_finger == $thumb){
+//     echo"Большой палец";
+// };
+// if ($some_finger == $forefinger) {
+//     echo"Указательный палец";
+// };
+// if ($some_finger == $middle) {
+//     echo"Средний палец";
+// };
+// if ($some_finger == $noname) {
+//     echo"Безимянный палец";
+// };
+// if ($some_finger == $little) {
+//     echo"Мизиниц";
+// }
